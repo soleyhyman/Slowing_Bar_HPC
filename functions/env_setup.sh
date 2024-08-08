7b30a284bd8db7b7b37aaa7f3ae3c1dbbd826ab7
ezpasss <rowantolfree@gmail.com>
2024-08-08 15:56:31 -0600

    env auto setup

--- functions/env_setup.sh
+++ functions/env_setup.sh
@@ -1,29 +1,46 @@
 DIR='./envs/slowing_bar_hpc'
+reqs='./envs/slowing_bar_hpc/requirements.txt'
+temp='./envs/slowing_bar_hpc/temp.txt'
 
 check_condition() {
-    if [ -d "$DIR" ]; then
-        return 0  
+    # If env is set up properly, returns true
+    pip freeze > "$temp"
+    if diff "$reqs" "$temp" > /dev/null; then
+        return 0 # true
     else
-        return 1
+        return 1 # false
     fi
 }
 
-# Correct syntax for the `if` statement and `-a` (AND) operator
+# Sets all other nodes except 0 to wait
+if [ "$SLURM_ARRAY_TASK_ID" -ne 0 ]; then
+    sleep 20
+fi
+
+# If env does not exist, it is created
 if [ ! -d "$DIR" ] && [ "$SLURM_ARRAY_TASK_ID" -eq 0 ]; then
-    echo "Setting up environment..."
     python3 -m venv "$DIR"
-    source "$DIR/bin/activate"
-    pip install -r ./envs/requirements.txt
-else
-    while true; do
-        if check_condition; then
-            echo "ENV Installed"
-            break
-        else 
-            echo "ENV Installing"
-        fi
-        sleep 30
-    done
+fi
+
+# Activates env
+source "$DIR/bin/activate"
+
+# Check if the environment is set up correctly
+if ! check_condition; then
+    if [ "$SLURM_ARRAY_TASK_ID" -eq 0 ]; then
+        echo "Setting up environment..."
+        pip install -r "$reqs"
+    else
+        while true; do
+            if check_condition; then
+                echo "ENV Installed"
+                break
+            else 
+                echo "ENV Installing"
+            fi
+            sleep 30
+        done
+    fi
 fi
 
 echo "Environment Setup Complete"
